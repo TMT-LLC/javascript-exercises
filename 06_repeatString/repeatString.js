fe2860814e7643b86202ea0650a7969034b1f16a
Richard Harris <245955565+TMT-LLC@users.noreply.github.com>
2025-12-18 18:00:47 -0500

    Relearn loops and understanding of function params
    After a much needed review, I was able to successfully
    impliment the requeired for loop using i to start
    at the first index [0], check the i variable to be
    valid in regards to the parameter of 'num', and increment
    itself after each successful loop by 1 (i++).
    
    Furthermore, using an empty string variable (phrase),
    I was able to learn how an accumulator variable can
    be useful especially in a scenario such as this.
    
    Moving onto the next exercise.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,24 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    // Need a variable that starts empy, adds 'str' to it, after 
+    // loop will return the result
+    // This is also known as an accumulator
+    let phrase = "";
 
+    if (num < 0) {
+        return phrase += "ERROR"
+    } else {
+
+    for (let i = 0; i < num; i++){
+        // i = 0 -> starts at the first index (0)
+        // i < greeting -> keep looping while i is a valid index
+        // i++ -> move to the next index after each loop
+
+        // Add whatever str to empy phrase variable num times
+        phrase += str
+    }
+    // Return ends the function/loop
+    return phrase
+    }
 };
 
 // Do not edit below this line
